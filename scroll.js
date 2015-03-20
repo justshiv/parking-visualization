cb62e5f4fad114b4d81d4cdd69bb81697ef5e88f
siobhan-odonovan <siobhan.odonovan@hotmail.com>
2015-03-20 16:28:28 +0200

    Initialised scrolling website skeleton

---
+++ scroll.js
@@ -0,0 +1,24 @@
+/**
+ * Created by siobhan on 15/03/20.
+ */
+
+//code from: http://www.1stwebdesigner.com/create-scrolling-parallax-website/
+
+$(document).ready(function() {
+    $('a[href*=#]').each(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname && this.hash.replace(/#/,'') ) {
+            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+            if ($target) {
+                var targetOffset = $target.offset().top;
+                $(this).click(function() {
+                $("#nav li a").removeClass("active");
+                $(this).addClass('active');
+                $('html, body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+                });
+            }
+        }
+    });
+
+});
